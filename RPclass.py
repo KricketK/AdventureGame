f55f96f6f1c586b4082507df2759136f27b82e8b
KRICKET <kricketkeeling@gmail.com>
2015-09-15 19:25:19 -0700

    Beginning attempts after first fail

---
+++ RPclass.py
@@ -0,0 +1,19 @@
+__author__ = 'KRISTINE'
+
+import random
+
+#Character basic needs
+#class basic needs
+    #HP, Attack, Defense, (Special Abilities, Character Description)
+#classes: Fighter, Barbarian, Assassin, Wizard, Mage, Shaman, Alchemist
+
+
+class Character(object):
+    def __init__(self, name=raw_input("What is your name?"), health=10, attack=5, defense=5):
+        self.name = name
+        self.health = health
+        self.attack = attack
+        self.defense = defense
+
+    def fight(self, damage):
+        self.damage = enemy_attack - self.defense
