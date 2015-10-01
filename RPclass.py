407020f8fbf5e3e7392394c305a280c4a19f608f
KRICKET <kricketkeeling@gmail.com>
2015-10-01 16:32:35 -0700

    Beginning the game

--- RPclass.py
+++ RPclass.py
@@ -17,6 +17,7 @@ class Weapon(object):
         self.attack_power = 0
         self.attack_penalty = 0
 
+
 class Club(Weapon):
     def __init__(self):
         super(Club, self).__init__()
@@ -30,11 +31,13 @@ class Sword(Weapon):
         self.attack_power = 5
         self.attack_penalty = 1
 
+
 class Character(object):
 
-    def __init__(self, name, health=10, power=5, defense=5, weapon=Weapon()):
+    def __init__(self, name, special, health=10, power=5, defense=5, weapon=Weapon()):
         self.weapon = weapon
         self.name = name
+        self.special = special
         self.health = health
         self.power = power
         self.defense = defense
@@ -42,11 +45,15 @@ class Character(object):
         self.proficiencies = []
 
     def __str__(self):
-        return "My name is " + self.name
+        return "My name is " + self.name + "Prepare yourself."
 
-    def add(self, other):
-        print("Health increased by " + str(other))
-        self.health += other
+    def add(self, stat, other):
+        print(str(stat) + " increased by " + str(other))
+        self.stat += other
+
+    def reduce(self, stat, other):
+        print(str(stat) + " decreased by " + str(other))
+        self.stat -= other
 
     def attack(self):
         hit_strength = random.randint(1, self.power)
@@ -56,7 +63,6 @@ class Character(object):
             hit_strength -= self.weapon.attack_penalty
             if hit_strength < 0:
                 hit_strength = 0
-        #do a check to make sure I'm not returning a negative number...
         return hit_strength
 
     def defend(self, attackpwr):
@@ -73,8 +79,14 @@ class Wizard(Character):
 class Mage(Wizard):
     def __init__(self, *args, **kwargs):
         super(Mage, self).__init__(*args, **kwargs)
-        self.proficiencies = ['wand', 'staff', 'broomstick', 'sword']
+        self.proficiencies.append('sword')
 
+
+class Shaman(Wizard):
+    def __init__(self, *args, **kwargs):
+        super(Shaman, self).__init__(*args, **kwargs)
+        self.proficiencies.extend(['familiar', 'knife'])
+        self.proficiencies.remove('broomstick')
 
 class Fighter(Character):
     def __init__(self, *args, **kwargs):
@@ -87,6 +99,27 @@ class Barbarian(Fighter):
         super(Barbarian, self).__init__(*args, **kwargs)
         self.proficiencies.append('club')
 
+
+class Assassin(Fighter):
+    def __init__(self, *args, **kwargs):
+        super(Assassin, self).__init__(*args, **kwargs)
+        self.proficiencies.extend(['dart', 'poison'])
+        self.proficiencies.remove('axe')
+
+
+class Scholar(Character):
+    def __init__(self, *args, **kwargs):
+        super(Scholar,self).__init__(*args, **kwargs)
+        self.proficiencies = ['knife', 'pen']
+        self.special = self.damage + random.randint(1, 3)
+
+
+class Alchemist(Scholar):
+    def __init__(self, *args, **kwargs):
+        super(Alchemist, self).__init__(*args, **kwargs)
+        self.proficiencies.extend(['bombs', 'elixirs', 'poison', 'hyde'])
+        self.special = self.damage
+
 def rollClass(character_type, name):
     return character_type(name, health=random.randint(1, 15), defense=random.randint(1, 7))
 
@@ -94,8 +127,13 @@ print("Welcome to the arena!")
 
 print("Here is your opponent!")
 
-Hickory = Mage('Hickory', 13, 8, 3, 'sword')
+Hickory = Barbarian('Hickory', 13, 8, 3, 'poison')
+print Hickory
 
+raw_input('What path do you choose?'
+      '1. Fighter'
+      '2. Wizard'
+      '3. Scholar')
 
 
 
